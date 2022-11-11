d356798a7a250db4dc3637361d7cc52ed039821b
saramsa2 <hwangc10@myunitec.ac.nz>
2022-11-12 00:07:13 +1300

    Set up enviroment

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            Assignment 2 Homepage
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/Semester.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Semester(props) {
+    return (
+        <div>
+            Semester Page
+        </div>
+    );
+}
+
+export default Semester;
